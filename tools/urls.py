1e24d5710456cddc1608f14d755b2a155d6fb15f
Thomas Walter <speisekatze@users.noreply.github.com>
2020-06-03 14:08:04 +0200

    +navbar

--- tools/urls.py
+++ tools/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('passwd/', include('passwd.urls')),
+    path('dba/', include('dba.urls')),
     path('', include('dba.urls')),
     path('admin/', admin.site.urls),
 ]
