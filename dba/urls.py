5829c6db98cae7c4ea369816fd948568981f4eb2
Thomas Walter <speisekatze@users.noreply.github.com>
2020-06-11 23:14:38 +0200

    fixing url bug

--- dba/urls.py
+++ dba/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'dba'
 urlpatterns = [
     path('', views.DbListView.as_view(), name='index'),
+    path('dba/', views.DbListView.as_view(), name='index'),
     path('newdb/<str:host>', views.NewDBView.as_view(), name='newdb'),
     path('newdb/<str:host>/<str:instance>', views.NewDBView.as_view(), name='newdb'),
 ]
--- tools/urls.py
+++ tools/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('passwd/', include('passwd.urls')),
-    path('dba/', include('dba.urls')),
     path('', include('dba.urls')),
     path('admin/', admin.site.urls),
 ]
