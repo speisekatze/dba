861a6dc0847d92ce53d937dcd8adf3cb4d1ff39b
Thomas Walter <41067636+speisekatze@users.noreply.github.com>
2020-06-22 07:47:46 +0200

    LDAP model

--- dba/models.py
+++ dba/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class User(AbstractUser):
+    is_dsb = models.BooleanField(default=False)
+    is_gf = models.BooleanField(default=False)
