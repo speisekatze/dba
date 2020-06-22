466dca458c7d52fa446171ff3b633b2e7f3d42b2
Thomas Walter <41067636+speisekatze@users.noreply.github.com>
2020-06-22 14:27:28 +0200

    url fix2

--- passwd/urls.py
+++ passwd/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'passwd'
 urlpatterns = [
     path('', views.PasswdView.as_view(), name='index'),
-    path('ergebniss', views.PasswdView.as_view(), name='index'),
+    path('ergebniss', views.PasswdView.as_view(), name='ergebniss'),
 ]
