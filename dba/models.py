a1463aa99aa7c2513bb8e042cee2e133528fcbba
Administrator <Administrator@logodata.intern>
2020-12-11 17:20:06 +0100

    objectguid from ldap in model

--- dba/models.py
+++ dba/models.py
@@ -6,3 +6,4 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     is_dsb = models.BooleanField(default=False)
     is_gf = models.BooleanField(default=False)
+    objid = models.BinaryField(default=False, max_length=100)
