aff82e732f20f03a4e3d77f446160a1b92ee0c45
Administrator <Administrator@logodata.intern>
2020-12-11 17:18:06 +0100

    success page for new db

--- dba/urls.py
+++ dba/urls.py
@@ -6,6 +6,8 @@ app_name = 'dba'
 urlpatterns = [
     path('', views.DbListView.as_view(), name='index'),
     path('dba/', views.DbListView.as_view(), name='index'),
+    path('newdb/success/<int:dbtid>', views.NewDBSuccess.as_view(), name='newdbsuccess'),
     path('newdb/<str:host>', views.NewDBView.as_view(), name='newdb'),
     path('newdb/<str:host>/<str:instance>', views.NewDBView.as_view(), name='newdb'),
+    
 ]
