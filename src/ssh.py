4a0e431ca123934b6cfe75c750f34bd2e2fc9aaf
Administrator <Administrator@logodata.intern>
2020-12-23 10:09:51 +0100

    simple paramiko wrapper

---
+++ src/ssh.py
@@ -0,0 +1,24 @@
+import paramiko
+from django.conf import settings
+from src.config import config
+
+class ssh:
+    conf = config(settings.DBA_CONFIG_FILE)
+    connection = None
+    host = None
+
+    def __init__(self, host):
+        self.host = host
+        self.conf = config(settings.DBA_CONFIG_FILE)['dba']['ssh'][self.host]
+
+    def open(self):
+        self.connection = paramiko.SSHClient()
+        self.connection.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.connection.connect(self.conf['host'], username=self.conf['user'], password=self.conf['pass'])
+
+    def cmd(self, command):
+        stdin, stdout, stderr = self.connection.exec_command(command, get_pty=True)
+        stdin.channel.shutdown_write()
+        messages = [x for x in stdout]
+        error = [x for x in stderr]
+        return messages, error
