1ff163a89888c235a77aec134381c3be1ccf8e9f
speisekatze <speisekatze@gmail.com>
2021-02-17 14:18:44 +0100

    fix for new entry in dbt fix for adding environments with oracle or mysql databases

--- dba/views.py
+++ dba/views.py
@@ -47,43 +47,75 @@ class NewDBView(generic.FormView):
 
     def form_valid(self, form):
         dbt = general.connect_dbt(self.conf)
-        # grund_id, eingespielt_von, eingespielt_am, verschluesselt, passwort, ziel_db, umgebung_dev, umgebung_beta, umgebung_release, createuser, createtime
+        ldap_user = ''.join(["%0.2x" % int(x) for x in self.request.user.objid])
+        # DBT Datenbank Eintrag
         data = {}
         data['kunden_id'] = form.cleaned_data['kunden']
-        data['grund_id'] = 1
+        data['grund_id'] = 3
         data['eingespielt_von'] = ''.join(["%0.2x" % int(x) for x in self.request.user.objid])
         data['eingespielt_am'] = int(datetime.now().timestamp())
         if form.cleaned_data['passwort'] != '':
             form.cleaned_data['verschluesselt'] = 'j'
             data['passwort'] = form.cleaned_data['passwort']
-        data['ziel_db'] = form.cleaned_data['host_name'] + '\\' + form.cleaned_data['instance_name'] + ' - ' + form.cleaned_data['db_name']
+        data['ziel_db'] = form.cleaned_data['host_name']
+        if form.cleaned_data['host_driver'].upper() != 'MYSQL':
+            data['ziel_db'] += '\\' + form.cleaned_data['instance_name']     
+        data['ziel_db'] += ' - ' + form.cleaned_data['db_name']
         if 'dev' in form.cleaned_data['server']:
             data['umgebung_dev'] = form.cleaned_data['u_name']
-            self.umgebung_anlegen(self.conf['dba']['lde']['dev'], form)
+            self.umgebung_anlegen(self.conf['dba']['lde']['dev'], 545, form)
         if 'beta' in form.cleaned_data['server']:
             data['umgebung_beta'] = form.cleaned_data['u_name']
-            self.umgebung_anlegen(self.conf['dba']['lde']['beta'], form)
+            self.umgebung_anlegen(self.conf['dba']['lde']['beta'], 472, form)
         if 'release' in form.cleaned_data['server']:
             data['umgebung_release'] = form.cleaned_data['u_name']
-            self.umgebung_anlegen(self.conf['dba']['lde']['release'], form)
-        data['createuser'] = ''.join(["%0.2x" % int(x) for x in self.request.user.objid])
+            self.umgebung_anlegen(self.conf['dba']['lde']['release'], 203, form)
+        data['createuser'] = ldap_user
         data['createtime'] = int(datetime.now().timestamp())
-        #print(data)
+        data['geplante_vern_am'] = int(datetime.now().timestamp()) + (14*86400)
+        data['tatsaechliche_vern_am'] = 0
+        data['rueckmeldung_kunde_am'] = 0
+        data['ruecksendung_am'] = 0
+        data['bereitgestellt_am'] = int(datetime.now().timestamp())
         id = mysql.write_data_single(dbt, 'Datenbank', data)
+        # DBT Historie Eintrag
+        hist = {}
+        hist['kunde'] = form.cleaned_data['kunden']
+        hist['datenbank'] = id
+        hist['alt'] = ''
+        hist['neu'] = ''
+        hist['aenderungsgrund'] = 'Neuanlage'
+        hist['zusammenfassung'] = ''
+        hist['benutzer'] = ldap_user
+        hist['aenderungszeit'] = int(datetime.now().timestamp())
+        mysql.write_data_single(dbt, 'Historie', hist)
         self.import_db(form.cleaned_data)
         self.success_url += '/'+str(id)
         return super(NewDBView, self).form_valid(form)
 
-    def umgebung_anlegen(self, db_file_name, form):
+    def umgebung_anlegen(self, db_file_name, id, form):
         lde_db = sqlite.create(db_file_name)
         umgebung = sqlite.write_data_single(lde_db, 'umgebung', {'umgebungs_name': form.cleaned_data['u_name']})
-        db = sqlite.query(lde_db, 'copy', [umgebung, 316], commit=True)
+        db = sqlite.query(lde_db, 'copy', [umgebung, id], commit=True)
 
-        update = { 'conf_value': form.cleaned_data['host_name'] + '\\' + form.cleaned_data['instance_name']}
+        if form.cleaned_data['host_driver'].upper() == 'MYSQL':
+            update = { 'conf_value': form.cleaned_data['host_name']}
+        else:
+            update = { 'conf_value': form.cleaned_data['host_name'] + '\\' + form.cleaned_data['instance_name']}
         sqlite.update(lde_db, 'lde_conf', {'ind_umgebung': umgebung, 'conf_int_name': 'tmp_db_host'}, update)
         
         update = { 'conf_value': form.cleaned_data['db_name']}
         sqlite.update(lde_db, 'lde_conf', {'ind_umgebung': umgebung, 'conf_int_name': 'tablespace'}, update)
+
+        update = { 'conf_value': form.cleaned_data['host_driver'].upper()}
+        sqlite.update(lde_db, 'lde_conf', {'ind_umgebung': umgebung, 'conf_int_name': 'tmp_database_type'}, update)
+        
+        if form.cleaned_data['host_driver'].upper() == 'ORACLE':
+            update = { 'conf_value': form.cleaned_data['db_name'].lower()}
+            sqlite.update(lde_db, 'lde_conf', {'ind_umgebung': umgebung, 'conf_int_name': 'db_user'}, update)
+        if form.cleaned_data['host_driver'].upper() == 'MYSQL':
+            update = { 'conf_value': 'root'}
+            sqlite.update(lde_db, 'lde_conf', {'ind_umgebung': umgebung, 'conf_int_name': 'db_user'}, update)
         
         update = { 'conf_value': form.cleaned_data['kunden_name'].lower()}
         sqlite.update(lde_db, 'lde_conf', {'ind_umgebung': umgebung, 'conf_int_name': 'kassenSchnittstelle'}, update)
@@ -108,22 +140,24 @@ class NewDBView(generic.FormView):
         initial = super(NewDBView, self).get_initial()
         initial.update(get_default_data())
         initial['seite'] = 'Neue Datenbank'
-        initial['host_name'] = self.kwargs['host']
+        host_name = self.kwargs['host']
         initial['instance_name'] = self.kwargs['instance']
         #initial['form'] = self.form_class(self.kwargs)
         initial['backlink'] = '/dba/'
-        initial['host_driver'] = [x for x in self.conf['dba']['database']['hosts'] if initial['host_name'] in x.values()][0]['driver']
+        initial['host_driver'] = [x for x in self.conf['dba']['database']['hosts'] if host_name in x.values()][0]['driver']
+        initial['host_name'] = [x for x in self.conf['dba']['database']['hosts'] if host_name in x.values()][0]['fqdn']
         return initial
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context.update(get_default_data())
         context['seite'] = 'Neue Datenbank'
-        context['host_name'] = self.kwargs['host']
+        host_name = self.kwargs['host']
         context['instance_name'] = self.kwargs['instance']
         #context['form'] = self.form_class(self.kwargs)
         context['backlink'] = '/dba/'
-        context['host_driver'] = [x for x in self.conf['dba']['database']['hosts'] if context['host_name'] in x.values()][0]['driver']
+        context['host_driver'] = [x for x in self.conf['dba']['database']['hosts'] if host_name in x.values()][0]['driver']
+        context['host_name'] = [x for x in self.conf['dba']['database']['hosts'] if host_name in x.values()][0]['fqdn']
         return context
 
 class NewDBSuccess(generic.TemplateView):
@@ -176,20 +210,38 @@ class DbListView(generic.FormView):
             connection = dblist.get_db_connection()
             dblist.db.drop(connection, request.POST.get('db_name'))
             show_list = True
+        if (request.POST.get('remove_env')):
+            dev_id = request.POST.get('devid')
+            beta_id = request.POST.get('betaid')
+            release_id = request.POST.get('releaseid')
+            envid = request.POST.get('envid')
+            envserver = request.POST.get('envserver')
+            if dev_id and beta_id and release_id:
+                if dev_id > 0:
+                    general.umgebung_loeschen(self.conf, 'dev', dev_id)
+                if beta_id > 0:
+                    general.umgebung_loeschen(self.conf, 'beta', beta_id)
+                if release_id > 0:
+                    general.umgebung_loeschen(self.conf, 'release', release_id)
+            if envid and envserver:
+                general.umgebung_loeschen(self.conf, envserver, envid)
+            show_list = True
         if (request.POST.get('shrink')):
             connection = dblist.get_db_connection()
             dblist.db.query(connection, 'shrink', request.POST.get('db_name'))
             show_list = True
         if (request.POST.get('simple')):
             connection = dblist.get_db_connection()
-
             dblist.db.query_format(connection, 'simple', request.POST.get('db_name'))
             show_list = True
         if (request.POST.get('select') == 'Auswahl'):
             context['debugmessage'] = ''
         if (request.POST.get('suchen') == 'Suchen' or show_list):
+            print('show')
             context['seite'] = self.seite
-            context['db_list'] = self.suchen(dblist)
+            zeug = self.suchen(dblist)
+            context['db_list'] = zeug[0]
+            context['env_list'] = zeug[1]
             context['db_driver'] = dblist.driver
         return render(request, self.template_name, context)
 
@@ -204,12 +256,27 @@ class DbListView(generic.FormView):
         vernichten = general.load_dbt(dbt)
         db_tuples = dblist.get_db_list()
         db_list = []
+        found_dev = []
+        found_beta = []
+        found_release = []
         for db in db_tuples:
             items = {}
             info = general.find_in_dbt(vernichten, dblist.instance_name, db[0])
-            items['umgebung'] = 'DEV: ' + general.umgebung_suchen(dev, db[0])
-            items['umgebung'] += "\r\n" + 'BETA: ' + general.umgebung_suchen(beta, db[0])
-            items['umgebung'] += "\r\n" + 'RELEASE: ' + general.umgebung_suchen(release, db[0])
+            env_dev = general.umgebung_suchen(dev, db[0])
+            env_beta = general.umgebung_suchen(beta, db[0])
+            env_release = general.umgebung_suchen(release, db[0])
+            items['umgebung'] = 'DEV: ' + env_dev[0][0]
+            items['umgebung'] += "\r\n" + 'BETA: ' + env_beta[0][0]
+            items['umgebung'] += "\r\n" + 'RELEASE: ' + env_release[0][0]
+            items['devid'] = env_dev[0][1]
+            for env in env_dev:
+                found_dev.append(env[1])
+            items['betaid'] = env_beta[0][1]
+            for env in env_beta:
+                found_beta.append(env[1])
+            items['releaseid'] = env_release[0][1]
+            for env in env_release:
+                found_release.append(env[1])
             items['name'] = db[0]
             items['size'] = db[1]
             items['log'] = db[2]
@@ -226,4 +293,17 @@ class DbListView(generic.FormView):
                 items['delete'] = 'nicht gefunden'
                 items['delta'] = -9000
             db_list.append(items)
-        return db_list
+        unused = []
+        for env in dev:
+            if env[2] not in found_dev:
+                i = {'name': env[1].decode("cp1252"), 'id': env[2], 'server': 'dev'}
+                unused.append(i)
+        for env in beta:
+            if env[2] not in found_beta:
+                i = {'name': env[1].decode("cp1252"), 'id': env[2], 'server': 'beta'}
+                unused.append(i)
+        for env in release:
+            if env[2] not in found_release:
+                i = {'name': env[1].decode("cp1252"), 'id': env[2], 'server': 'release'}
+                unused.append(i)
+        return (db_list, unused)
